be0f0b09f64135326831a47d6a4b9a4585b4140a
Andrea Lai <alai.arpas@gmail.com>
2022-10-28 10:13:53 +0200

    basic-astro 0.0.1

--- cmd.sh
+++ cmd.sh
@@ -1,11 +1,14 @@
-docker build . -t poa-astro:0.0.11
+docker build . -t basic-astro:0.0.1
 
-docker run -it --rm  --env-file .env.list -p 8999:80 poa-astro:0.0.4
+docker run -it --rm  --env-file .env.list -p 8999:80 basic-astro:0.0.1
 
-docker tag poa-astro:0.0.11 registry.digitalocean.com/docean-alai-arpas/poa-astro:0.0.11
+docker tag basic-astro:0.0.1 registry.digitalocean.com/docean-alai-arpas/basic-astro:0.0.1
 
 
-docker push registry.digitalocean.com/docean-alai-arpas/poa-astro:0.0.11
+docker push registry.digitalocean.com/docean-alai-arpas/basic-astro:0.0.1
 
 kubectl apply -f k8_astro.yaml
+
+
+npm run dev
 
