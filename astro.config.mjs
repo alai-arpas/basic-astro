8bf1af1a32c30053fb8792e611a73a823d862b8a
Andrea Lai <alai.arpas@gmail.com>
2022-11-02 11:56:34 +0100

    add alpine

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import alpinejs from "@astrojs/alpinejs";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [alpinejs()]
+});
